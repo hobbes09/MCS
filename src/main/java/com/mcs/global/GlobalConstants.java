4c15f44fb9fec9b6dc4ae22ea58f0ab38642a2d1
hobbes09 <sourin.sutradhar@gmail.com>
2015-10-02 22:30:20 +0530

    Global constant / Config file

---
+++ src/main/java/com/mcs/global/GlobalConstants.java
@@ -0,0 +1,13 @@
+package com.mcs.global;
+
+public class GlobalConstants {
+	
+	public static String smsBaseScheme = "http";
+	public static String smsBaseUrl = "alerts.solutionsinfini.com/api/v3/index.php";
+	public static String smsSender = "SIDEMO";
+	public static String smsMethod = "sms";
+	public static String smsApiKey = "Ae563a81daa33ec8fb62cdece93358d94";
+	
+	
+	
+}
