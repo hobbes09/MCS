2aedd8fc296554e22021d3b9c55a26b0e5e65a29
hobbes09 <sourin.sutradhar@gmail.com>
2015-10-03 08:30:05 +0530

    Bug fix

--- src/main/java/com/mcs/repositories/OtpSmsReportRepository.java
+++ src/main/java/com/mcs/repositories/OtpSmsReportRepository.java
@@ -1,11 +1,13 @@
 package com.mcs.repositories;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.mcs.domains.OtpSmsReport;
 
 public interface OtpSmsReportRepository extends MongoRepository<OtpSmsReport, String>{
 	
-	public OtpSmsReport findByAgentSmsId(String agentSmsId);
+	public List<OtpSmsReport> findByAgentSmsId(String agentSmsId);
 	
 }
